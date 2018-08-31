dddd4b4112d4c1d3aaf6ce3a33381d9a7faddf20
Stephen Short <16711735+ducksrfr@users.noreply.github.com>
2018-08-30 21:05:58 -0500

    updates to sysadminctl

--- scripts/admin_pwreset.sh
+++ scripts/admin_pwreset.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Stephen Short, Bazaarvoice
-# This script uses the password passthrough option "-" so that a plaintext password is not permanently stored
-# The resetPasswordFor option is useful if you don't know the original account password
-# The interactive argument is required as sysadminctl will not run as root. The current logged in admin password is required.
-# You can add killall Terminal to the end if desired if you save this script as a dot command file
+# The resetPasswordFor option will always create a new Keychain for the user
 
-sysadminctl interactive -resetPasswordFor AccountNameHere -newPassword -
-exit 0
+sysadminctl -adminUser AdminUserHere -adminPassword AdminPasswordHere -resetPasswordFor UserToBeReset -newPassword NewPasswordForUser
