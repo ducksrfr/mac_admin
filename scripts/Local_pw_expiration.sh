9c398fee0bbf39ff7771b2eb63e7458b776ebad4
Stephen Short <16711735+ducksrfr@users.noreply.github.com>
2019-02-16 14:35:25 -0600

    version 1.1 update
    -Takes users directly to the Change Password dialog
    -Adjusted echo messages for clarity
    -Extended sleep time before recon

--- scripts/Local_pw_expiration.sh
+++ scripts/Local_pw_expiration.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
-# This script is intended for local user accounts with a password policy not managed by Active Directory or another IdP.
+# version 1.1
+# This script is intended for local user accounts with a password policy not managed by Active Directory, an IdP, or something like NoMAD.
 # If you're using a configration profile to manage the local user password age, the user will never receive proactive password expiration warnings.
-# A user might receive an expiration warning at the login screen, but not proactively in an active user session.
+# A user might receive an expiration warning at the login screen, but not in an active user session.
 # This script pulls the current logged in user's password age from a Jamf extended attribute titled "Password Age"
 # https://github.com/jamf/Current-User-Password-Age
 # The extended attribute gets updated based on your inventory collection frequency.
-# Add this script as a recurring check-in policy that executes once per day
+# Add this script as a recurring check-in policy that executes once per day.
 # If you don't use Jamf you can incorporate their script into this one to pull the password age value.
-# A user is simply presented with the option to open the Users & Groups preference pane to change their password. I purposely do not use dscl to change the password, as that can cause issues with secureToken in macOS High Sierra and Mojave.
+# A user is simply presented with the Change Password dialog in the Users & Groups prefpane. 
+# I don't use dscl to change the password, because that causes issues with secureToken in macOS High Sierra and Mojave.
 
 currentUser=$(scutil <<< "show State:/Users/ConsoleUser" | awk -F': ' '/[[:space:]]+Name[[:space:]]:/ { if ( $2 != "loginwindow" ) { print $2 }}')
 getUID=$(id -u $currentUser)
 
 # Exit if user is a network account
-if [[ $getUID -gt 1000 ]]; then
+if [[ $getUID -gt 999 ]]; then
   echo "Account is a network user, stopping expiration check."
   exit 1
 fi
@@ -32,10 +34,10 @@ if [ '$passwordAge -le 59' ];then
 	echo "The password for $currentUser is outside the warning period."
 	exit 1
 elif [ $passwordAge == 60 ];then
-	echo "Your password expires in 30 days."
+	echo "The password for $currentUser expires in 30 days."
 	/usr/bin/osascript -e 'Tell current application to display dialog "Your password expires in 30 days!
 	
-	" with icon "Macintosh HD:System:Library:CoreServices:CoreTypes.bundle:Contents:Resources:UserIcon.icns" as alias with title "Password Expiration Warning" with text buttons {"Maybe Later", "Change Password"} cancel button 1 default button 2' -e 'tell application "Finder" to open POSIX file "/System/Library/PreferencePanes/Accounts.prefPane"'
+	" with icon "Macintosh HD:System:Library:CoreServices:CoreTypes.bundle:Contents:Resources:UserIcon.icns" as alias with title "Password Expiration Warning" with text buttons {"Maybe Later", "Change Password"} cancel button 1 default button 2' -e 'tell application "Finder" to open POSIX file "/System/Library/PreferencePanes/Accounts.prefPane"' -e 'tell application "System Events" to tell process "System Preferences" to delay 1' -e 'tell application "System Events" to tell process "System Preferences" to click button "Change Password…" of tab group 1 of window "Users & Groups"'
 elif [ '$passwordAge -ge 61 -a $passwordAge -le 78' ];then
 	echo "The password for $currentUser is outside the warning period."
 	exit 1
@@ -43,35 +45,35 @@ elif [ $passwordAge -ge 79 -a $passwordAge -le 85 ];then
 	echo "The password for $currentUser expires soon!"
 	/usr/bin/osascript -e 'Tell current application to display dialog "Your password expires soon!
 	
-	" with icon "Macintosh HD:System:Library:CoreServices:CoreTypes.bundle:Contents:Resources:UserIcon.icns" as alias with title "Password Expiration Warning" with text buttons {"Maybe Later", "Change Password"} cancel button 1 default button 2' -e 'tell application "Finder" to open POSIX file "/System/Library/PreferencePanes/Accounts.prefPane"'
+	" with icon "Macintosh HD:System:Library:CoreServices:CoreTypes.bundle:Contents:Resources:UserIcon.icns" as alias with title "Password Expiration Warning" with text buttons {"Maybe Later", "Change Password"} cancel button 1 default button 2' -e 'tell application "Finder" to open POSIX file "/System/Library/PreferencePanes/Accounts.prefPane"' -e 'tell application "System Events" to tell process "System Preferences" to delay 1' -e 'tell application "System Events" to tell process "System Preferences" to click button "Change Password…" of tab group 1 of window "Users & Groups"'
 elif [ $passwordAge == 86 ];then
 	echo "The password for $currentUser expires in 3 days!"
 	/usr/bin/osascript -e 'Tell current application to display dialog "Your password expires in 3 days!
 	
-	Avoid account lockout issues by changing your password now." with icon "Macintosh HD:System:Library:CoreServices:CoreTypes.bundle:Contents:Resources:AlertCautionIcon.icns" as alias with title "Password Expiration Warning" with text buttons {"Maybe Later", "Change Password"} cancel button 1 default button 2' -e 'tell application "Finder" to open POSIX file "/System/Library/PreferencePanes/Accounts.prefPane"'
+	Avoid account lockout issues by changing your password now." with icon "Macintosh HD:System:Library:CoreServices:CoreTypes.bundle:Contents:Resources:AlertCautionIcon.icns" as alias with title "Password Expiration Warning" with text buttons {"Maybe Later", "Change Password"} cancel button 1 default button 2' -e 'tell application "Finder" to open POSIX file "/System/Library/PreferencePanes/Accounts.prefPane"' -e 'tell application "System Events" to tell process "System Preferences" to delay 1' -e 'tell application "System Events" to tell process "System Preferences" to click button "Change Password…" of tab group 1 of window "Users & Groups"'
 elif [ $passwordAge == 87 ];then
 	echo "The password for $currentUser expires in 2 days!"
 	/usr/bin/osascript -e 'Tell current application to display dialog "Your password expires in 2 days!
 	
-	Avoid account lockout issues by changing your password now." with icon "Macintosh HD:System:Library:CoreServices:CoreTypes.bundle:Contents:Resources:AlertCautionIcon.icns" as alias with title "Password Expiration Warning" with text buttons {"Maybe Later", "Change Password"} cancel button 1 default button 2' -e 'tell application "Finder" to open POSIX file "/System/Library/PreferencePanes/Accounts.prefPane"'
+	Avoid account lockout issues by changing your password now." with icon "Macintosh HD:System:Library:CoreServices:CoreTypes.bundle:Contents:Resources:AlertCautionIcon.icns" as alias with title "Password Expiration Warning" with text buttons {"Maybe Later", "Change Password"} cancel button 1 default button 2' -e 'tell application "Finder" to open POSIX file "/System/Library/PreferencePanes/Accounts.prefPane"' -e 'tell application "System Events" to tell process "System Preferences" to delay 1' -e 'tell application "System Events" to tell process "System Preferences" to click button "Change Password…" of tab group 1 of window "Users & Groups"'
 elif [ $passwordAge == 88 ];then
 	echo "The password for $currentUser expires in 1 day!"	
 	/usr/bin/osascript -e 'Tell current application to display dialog "Your password expires tomorrow!
 	
-	Avoid account lockout issues by changing your password now." with icon "Macintosh HD:System:Library:CoreServices:CoreTypes.bundle:Contents:Resources:AlertCautionIcon.icns" as alias with title "Password Expiration Warning" with text buttons {"Maybe Later", "Change Password"} cancel button 1 default button 2' -e 'tell application "Finder" to open POSIX file "/System/Library/PreferencePanes/Accounts.prefPane"'
+	Avoid account lockout issues by changing your password now." with icon "Macintosh HD:System:Library:CoreServices:CoreTypes.bundle:Contents:Resources:AlertCautionIcon.icns" as alias with title "Password Expiration Warning" with text buttons {"Maybe Later", "Change Password"} cancel button 1 default button 2' -e 'tell application "Finder" to open POSIX file "/System/Library/PreferencePanes/Accounts.prefPane"' -e 'tell application "System Events" to tell process "System Preferences" to delay 1' -e 'tell application "System Events" to tell process "System Preferences" to click button "Change Password…" of tab group 1 of window "Users & Groups"'
 elif [ $passwordAge == 89 ];then
 	echo "The password for $currentUser expires today!"	
 	/usr/bin/osascript -e 'Tell current application to display dialog "Your password expires today!
 	
-	You must change your password at this time." with icon "Macintosh HD:System:Library:CoreServices:CoreTypes.bundle:Contents:Resources:AlertStopIcon.icns" as alias with title "Password Expiration Warning" with text buttons {"Change Password"} default button 1' -e 'tell application "Finder" to open POSIX file "/System/Library/PreferencePanes/Accounts.prefPane"'
+	You must change your password at this time." with icon "Macintosh HD:System:Library:CoreServices:CoreTypes.bundle:Contents:Resources:AlertStopIcon.icns" as alias with title "Password Expiration Warning" with text buttons {"Change Password"} default button 1' -e 'tell application "Finder" to open POSIX file "/System/Library/PreferencePanes/Accounts.prefPane"' -e 'tell application "System Events" to tell process "System Preferences" to delay 1' -e 'tell application "System Events" to tell process "System Preferences" to click button "Change Password…" of tab group 1 of window "Users & Groups"'
 elif [ $passwordAge -ge 90 ];then
 	echo "The password for $currentUser has expired!"	
 	/usr/bin/osascript -e 'Tell current application to display dialog "Your password has expired!
 	
-	You must reset your password now to unlock your account." with icon "Macintosh HD:System:Library:CoreServices:CoreTypes.bundle:Contents:Resources:AlertStopIcon.icns" as alias with title "Password Expiration Warning" with text buttons {"Change Password"} default button 1' -e 'tell application "Finder" to open POSIX file "/System/Library/PreferencePanes/Accounts.prefPane"'
+	You must reset your password now to unlock your account." with icon "Macintosh HD:System:Library:CoreServices:CoreTypes.bundle:Contents:Resources:AlertStopIcon.icns" as alias with title "Password Expiration Warning" with text buttons {"Change Password"} default button 1' -e 'tell application "Finder" to open POSIX file "/System/Library/PreferencePanes/Accounts.prefPane"' -e 'tell application "System Events" to tell process "System Preferences" to delay 1' -e 'tell application "System Events" to tell process "System Preferences" to click button "Change Password…" of tab group 1 of window "Users & Groups"'
 fi
 
 # allow user time to change password before updating inventory to reflect new password age
-sleep 60
+sleep 90
 /usr/local/jamf/bin/jamf recon
 exit 0
