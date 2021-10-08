523f5a0e46f2a3c6c04c110d599c01b0b92fd975
Stephen Short <16711735+ducksrfr@users.noreply.github.com>
2021-10-08 14:18:59 -0500

    Create block-private-relay.sh
    Adjusts a user's hosts file to loopback to localhost for iCloud Private Relay https://developer.apple.com/support/prepare-your-network-for-icloud-private-relay/

---
+++ scripts/block-private-relay.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Block iCloud Private Relay
+# Stephen Short, 10/2021
+
+iCloud=$(cat /private/etc/hosts | grep icloud )
+loopbackAddress=$(cat /private/etc/hosts | grep localhost | awk '/[0-9].[0-9]/ {print $1}')
+
+
+if [[ $iCloud == "" ]]; then
+	echo "iCloud Private Relay is not blocked on this device!"
+	echo "$loopbackAddress mask.icloud.com" >> /private/etc/hosts
+	echo "$loopbackAddress mask-h2.icloud.com" >> /private/etc/hosts
+	/usr/local/bin/jamf policy -event flushDNS
+else
+	echo "iCloud Private Relay has already been added to the hosts file."
+fi
